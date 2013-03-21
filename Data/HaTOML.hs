24df8fd8b09ab0bc1ab4650918dc939cb3010821
kongo2002 <kongo2002@googlemail.com>
2013-03-21 23:36:09 +0100

    start implementing encoding

--- Data/HaTOML.hs
+++ Data/HaTOML.hs
@@ -2,7 +2,10 @@ module Data.HaTOML where
 
 import qualified Data.Attoparsec.ByteString.Char8 as AB
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import           Data.ByteString.Lazy.Builder
 
+import Data.HaTOML.Encode
 import Data.HaTOML.Parser
 import Data.HaTOML.Types
 
@@ -11,3 +14,6 @@ parse bs =
     case AB.parseOnly toml bs of
       Left _  -> Nothing
       Right r -> Just r
+
+encode :: TOML -> LBS.ByteString
+encode = toLazyByteString . fromToml
---
+++ Data/HaTOML/Encode.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Data.HaTOML.Encode where
+
+import           Data.Monoid                        ( mappend, Monoid )
+import           Data.ByteString.Lazy.Builder
+import           Data.ByteString.Lazy.Builder.ASCII ( integerDec, doubleDec )
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Map as M
+import           Numeric                            ( showHex )
+
+import Data.HaTOML.Types
+
+
+fromToml :: TOML -> Builder
+fromToml = fromValue . TGroup
+
+
+fromValue :: TValue -> Builder
+fromValue (TBool b) =
+    stringUtf8 enc
+  where enc = if b then "true" else "false"
+fromValue (TInteger i) = integerDec i
+fromValue (TDouble d) = doubleDec d
+fromValue (TString s) =
+    charUtf8 '"' <> quote s <> charUtf8 '"'
+  where
+    quote str =
+        case BS.uncons t of
+          Nothing      -> byteString h
+          Just (c, t') -> byteString h <> stringUtf8 (escape c) <> quote t'
+      where
+        (h, t) = BS.break isEscape str
+    isEscape c = c == '\"' ||
+                 c == '\\' ||
+                 c < '\x20'
+    escape '\"' = "\\\""
+    escape '\\' = "\\\\"
+    escape '\n' = "\\n"
+    escape '\r' = "\\r"
+    escape '\t' = "\\t"
+    escape c
+        | c < '\x20' = "\\u" ++ replicate (4 - length hex) '0' ++ hex
+        | otherwise  = [c]
+        where hex = showHex (fromEnum c) ""
+fromValue (TGroup (TOML m)) =
+    M.foldrWithKey' proc (charUtf8 '\n') m
+  where
+    proc k v a = charUtf8 '\n' <> val k v <> a
+    val k v = byteString k <> charUtf8 '=' <> fromValue v
+
+
+infixr 4 <>
+(<>) :: Monoid m => m -> m -> m
+(<>) = mappend
