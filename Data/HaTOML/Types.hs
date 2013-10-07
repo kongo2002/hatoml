37f3f0f271ec3de224a584e6106e1c5ca757f0ad
kongo2002 <kongo2002@googlemail.com>
2013-10-08 00:05:07 +0200

    Data.Map: use qualified import instead

--- Data/HaTOML/Types.hs
+++ Data/HaTOML/Types.hs
@@ -8,11 +8,11 @@ module Data.HaTOML.Types
     ) where
 
 import qualified Data.ByteString.Char8 as BS
-import           Data.Map  ( Map, insert, insertWith, empty )
+import qualified Data.Map as M
 import           Data.Time ( UTCTime )
 
 newtype TOML =
-    TOML (Map BS.ByteString TValue)
+    TOML (M.Map BS.ByteString TValue)
     deriving ( Eq, Show, Ord )
 
 
@@ -26,17 +26,17 @@ data TValue = TString !BS.ByteString
               deriving ( Eq, Show, Ord )
 
 
-liftTOML :: (Map BS.ByteString TValue -> Map BS.ByteString TValue) -> TOML -> TOML
+liftTOML :: (M.Map BS.ByteString TValue -> M.Map BS.ByteString TValue) -> TOML -> TOML
 liftTOML f (TOML m) = TOML $ f m
 
 
 tinsert :: BS.ByteString -> TValue -> TOML -> TOML
-tinsert k v m = liftTOML (insert k v) m
+tinsert k v m = liftTOML (M.insert k v) m
 
 
 tinsertWith :: (TValue -> TValue -> TValue) -> BS.ByteString -> TValue -> TOML -> TOML
-tinsertWith f k v m = liftTOML (insertWith f k v) m
+tinsertWith f k v m = liftTOML (M.insertWith f k v) m
 
 
 tempty :: TOML
-tempty = TOML empty
+tempty = TOML M.empty
